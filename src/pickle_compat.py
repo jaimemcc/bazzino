355ae97c619b7a8f1be9e0bafde52a9d1764f902
jaimemcc <j.mccutcheon@uit.no>
2026-02-19 15:44:20 +0100

    fig2 fixed

--- src/pickle_compat.py
+++ src/pickle_compat.py
@@ -9,6 +9,35 @@ def enable_dill_pathlib_compat() -> None:
     if getattr(dill, "_pathlib_local_compat_enabled", False):
         return
 
+    # Patch pandas StringDtype to handle old pickle format
+    try:
+        import pandas as pd
+        from pandas.core.arrays.string_ import StringDtype as _OriginalStringDtype
+        
+        class StringDtypeCompat(_OriginalStringDtype):
+            def __new__(cls, *args, **kwargs):
+                # Old pandas StringDtype had a different signature
+                # New version only accepts 'storage' parameter
+                # Ignore extra positional arguments from old pickles
+                if len(args) > 1:
+                    args = args[:1]  # Keep only first argument (storage)
+                return super().__new__(cls)
+            
+            def __init__(self, *args, **kwargs):
+                # Old version accepted more arguments, new version accepts only storage
+                if len(args) > 1:
+                    # Ignore extra arguments from old pickle format
+                    args = (args[0],) if args else ()
+                super().__init__(*args, **kwargs)
+        
+        # Replace StringDtype in pandas modules
+        pd.StringDtype = StringDtypeCompat
+        pd.core.arrays.string_.StringDtype = StringDtypeCompat
+        sys.modules['pandas'].StringDtype = StringDtypeCompat
+        sys.modules['pandas.core.arrays.string_'].StringDtype = StringDtypeCompat
+    except (ImportError, AttributeError):
+        pass  # pandas not available or different structure
+
     original_load = dill.load
 
     def _dill_load_with_pathlib_compat(file_obj, *args, **kwargs):
