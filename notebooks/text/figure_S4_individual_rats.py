c90f5827259fc0dcb244dc5500f5f287d178f932
jaimemcc <j.mccutcheon@uit.no>
2026-06-16 16:34:51 +0200

    updated supp fig

--- notebooks/text/figure_S4_individual_rats.py
+++ notebooks/text/figure_S4_individual_rats.py
@@ -193,7 +193,8 @@ for idx, rat in enumerate(rats[:]):
     da_norm = norm_signal(auc_da, smooth=True)
     
     behav_transition = x_array.query("id == @rat and condition == 'deplete' and realigned_trials_behav == 0").trial.values[0]
-    print(f"Rat {rat} transition trial: {behav_transition}")
+    da_transition = x_array.query("id == @rat and condition == 'deplete' and realigned_trials_da == 0").trial.values[0]
+    print(f"Rat {rat} behav transition: {behav_transition}; DA transition: {da_transition}")
 
     vlim_behav = scale_vlim_to_data(behav_data, percentile=90)
     vlim_photo = scale_vlim_to_data(photo_data, percentile=90)
@@ -210,7 +211,7 @@ for idx, rat in enumerate(rats[:]):
 
     ax[idx, 1].plot(gaussian_filter1d(simba_median, sigma=1), color=colors[3])
     ax[idx, 1].set_ylim(-1.05, 1.05)
-    ax[idx, 1].axvline(behav_transition, color="green", linestyle="--", alpha=0.5)
+    ax[idx, 1].axvline(behav_transition, color="k", linestyle=":", alpha=0.5)
 
     hm = sns.heatmap(photo_data, ax=ax[idx, 3], cmap=custom_cmap,
                 vmin=vlim_photo[0], vmax=vlim_photo[1],
@@ -219,8 +220,10 @@ for idx, rat in enumerate(rats[:]):
     hm.collections[0].set_rasterized(True)
     ax[idx, 3].set_yticks([0, 49], labels=["0", "49"], rotation=0)
     ax[idx, 3].set_xticks([])
-    
+    ax[idx, 3].scatter(200, da_transition, color="k", marker="<", s=50, zorder=5, clip_on=False)
+
     ax[idx, 4].plot(da_norm, color=colors[3])
+    ax[idx, 4].axvline(da_transition, color="k", linestyle="--", alpha=0.5)
 
     for axis in [ax[idx, 1], ax[idx, 4]]:
         sns.despine(ax=axis, offset=5)
